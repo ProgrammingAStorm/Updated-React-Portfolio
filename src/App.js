3eea826d6dad8fb6ce87e7588ab218934af5244c
Mark Pavel <markpavel02@gmail.com>
2022-10-23 18:08:15 -0400

    AddedBasic Routing Structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <Header />
       <Banner />
-      
-      <BrowserRouter>
 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import { Container, Box, Heading } from "react-bulma-components";
+
+export default function Home() {
+    return (
+        <Container className="p-3">
+            <Heading>Welcome!</Heading>
+            <p>
+                This is my portfolio. I hope you enjoy what you see.
+                Everything you see is made by me with lots of love,
+                and is hosted on my decentralized NFT domain.
+            </p>
+            <br />
+            <p>
+                I hope you enjoy the toys that I have created
+                as much as I enjoyed creating them!
+            </p>
+        </Container>
+    );
+};
