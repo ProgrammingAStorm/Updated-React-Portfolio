59a12a46d4f760b1ae1cbaad122bcec4289c284d
Mark Pavel <markpavel02@gmail.com>
2022-10-23 17:21:46 -0400

    Added Header Structure

---
+++ src/components/Header.js
@@ -0,0 +1,62 @@
+import { Navbar, Heading } from 'react-bulma-components';
+import { useState } from 'react';
+
+export default function Header() {
+    const [burger, setBurger] = useState(false);
+    const [projects, setProjects] = useState(false);
+
+    return (
+        <Navbar active={burger}>
+            <Navbar.Brand>
+                <Navbar.Item className='p-5'>
+                    <div id="heart-shape"></div>
+                </Navbar.Item>
+
+                <Navbar.Item href="/">
+                    <Heading>
+                        ProgrammingAStorm
+                    </Heading>
+                </Navbar.Item>
+
+                <Navbar.Burger 
+                    onClick={ e => {
+                        setBurger(!burger)
+                    }}
+                />
+            </Navbar.Brand>
+
+            <Navbar.Menu>
+                <Navbar.Container>
+                    <Navbar.Item active={projects}>
+                        <Navbar.Link
+                            onClick={ e => {
+                                setProjects(!projects)
+                            }}
+                        >
+                            Projcts
+                        </Navbar.Link>
+                        <Navbar.Dropdown>
+                            <Navbar.Item href='/projects/bootcamp'>
+                                Bootcamp
+                            </Navbar.Item>
+                            <Navbar.Item href='/projects/personal'>
+                                Personal
+                            </Navbar.Item>
+                            <Navbar.Item href='/projects/toys'>
+                                Toys
+                            </Navbar.Item>
+                        </Navbar.Dropdown>
+                    </Navbar.Item>
+
+                    <Navbar.Item href='/about'>
+                        About Me
+                    </Navbar.Item>
+
+                    <Navbar.Item href='/contact'>
+                        Contact
+                    </Navbar.Item>
+                </Navbar.Container>
+            </Navbar.Menu>
+        </Navbar>
+    );
+};
